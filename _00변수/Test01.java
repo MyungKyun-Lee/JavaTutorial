5f2e8c12ad7faf99560847ed2339935c3ad1f92c
MyungKyun-Lee <goodb@naver.com>
2024-08-06 10:36:25 +0900

    00변수공부

---
+++ _00변수/Test01.java
@@ -0,0 +1,18 @@
+package _00변수;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 정수 변수 선언
+		int a = 10;
+		byte aa = 10;
+		System.out.println(a);
+		System.out.println(aa);
+		
+		//문자열 변수 선언
+		String s = "kimtr";
+		System.out.println(s);
+	}
+
+}
